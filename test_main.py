d999f593c178b7b6b02db098c735ade092b23d67
wtcc-wsg <wsgreco@my.waketech.edu>
2022-11-07 22:20:52 -0500

    fixes test_query_presidents

--- test_main.py
+++ test_main.py
@@ -8,8 +8,12 @@ def test_query_ddg():
 
 
 def test_query_presidents():
-    # Tests that the results where filtered correctly
-    presidents = query_presidents()
-    for president in presidents:
+    # Tests that all presidents where in response
+    presidents_dict = query_presidents()
+    seen = known_presidents.copy()
+    for president in presidents_dict:
         last_name = president["Text"].split(" - ")[0].lower().split(" ")[-1]
-        assert last_name in known_presidents
+        if last_name in seen:
+            seen.remove(last_name)
+
+    assert seen == []
